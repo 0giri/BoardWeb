e97d93d58d935e972db2608c177e1ee1020c27b1
0giri <higiri@naver.com>
2021-10-31 18:29:14 +0900

    단미 삭제 방지용 백

---
+++ src/main/java/com/springbook/view/user/LoginController.java
@@ -0,0 +1,32 @@
+package com.springbook.view.user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.springbook.biz.user.UserDTO;
+import com.springbook.biz.user.impl.UserDAO;
+import com.springbook.view.controller.Controller;
+
+public class LoginController implements Controller {
+
+	@Override
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("로그인 처리");
+
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+
+		UserDTO dto = new UserDTO();
+		dto.setId(id);
+		dto.setPwd(password);
+
+		UserDAO userDAO = new UserDAO();
+		UserDTO user = new UserDTO();
+
+		if (user != null) {
+			return "getBoardList.do";
+		} else {
+			return "login";
+		}
+	}
+}
